735129263e5af9515314732de747a52fdf510a92
Alex <alexander@nadjalin.com>
2021-05-27 15:44:51 +0200

    Refactoring. Bagging incomplete due to Keras.

--- DNN/dnn.py
+++ DNN/dnn.py
@@ -1,115 +1,88 @@
 # Dense Neural Network
-import pandas as pd
-
 from file_handling import file_handling as fh
 from data_handling import data_handling as dh
-import matplotlib.pyplot as plt
-import numpy as np
-import random
-import tensorflow as tf
-from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras.regularizers import l1,l2
-from tensorflow.keras.models import Sequential
+import dnn_model as dnnm
+import plotter as plt
+from tensorflow.keras.regularizers import l2
 from tensorflow.keras.optimizers import Adam
-from sklearn.metrics import accuracy_score
-from loguru import logger
 
 # Globals
 conf_file_name = 'C:\\Python projects\\ml_studio\\config.ini'
 file_name = 'ETF.csv'
 data_col = 'XACTOMXS30.ST_CLOSE'
-
-
-def create_model(optimizer, reg, hl=1, hu=128, dropout=True, rate=0.3, regularize=False):
-    # Regularization
-    if not regularize:
-        reg = None
-
-    model = Sequential()
-
-    # Default layer
-    model.add(Dense(hu, input_dim=len(cols), activation='relu', activity_regularizer=reg))
-
-    # Add dropout layer
-    if dropout:
-        model.add(Dropout(rate, seed=1000))
-    for _ in range(hl):
-        # Additional layer
-        model.add(Dense(hu, activation='relu', activity_regularizer=reg))
-
-        # Add dropout layer
-        if dropout:
-            model.add(Dropout(rate, seed=1000))
-        # Output layer
-        model.add(Dense(1, activation='sigmoid'))
-        # Loss function
-        model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-    return model
-
-
-def cw(df):
-    c0, c1 = np.bincount(df['d'])
-    w0 = (1 / c0) * len(df) / 2
-    w1 = (1 / c1) * len(df) / 2
-    return {0: w0, 1: w1}
-
-
-def set_seeds(seed=1000):
-    # Python numpy and tensorflow random seeds
-    random.seed(seed)
-    np.random.seed(seed)
-    tf.random.set_seed(seed)
+lags = 5
+optimizer = Adam(learning_rate=0.001)
+dropout = True
+regularize = True
+bagging = True
+max_features = 0.75
 
 
 if __name__ == '__main__':
+    # File imports
     conf = fh.config(conf_file_name)
     raw = fh.read_csv(conf, file_name)
 
+    # Select which column to use
     df = raw[data_col].to_frame()
 
-    ax = df[data_col].plot(figsize=(10, 6), title=data_col)
-    ax.set_ylabel('CLOSE')
-    plt.show()
+    # Add lagged features
+    data, cols = dh.add_lags(data=df,
+                             data_col=data_col,
+                             lags=lags)
 
-    lags = 5
-    data, cols = dh.add_lags(data=df, data_col=data_col, lags=lags)
-
-    # Standard optimizer
-    optimizer = Adam(learning_rate=0.001)
-
-    # Split the whole data set into training data and test data
-    split = int(len(data) * 0.8)
-    train = data.iloc[:split].copy()
-    test = data.iloc[split:].copy()
-
-    # Mean and standard deviation for all features
-    mu, std = train.mean(), train.std()
+    # Split into training and test data
+    train, test = dh.split_train_test(data)
 
     # Normalize training data
-    train_ = (train - mu) / std
+    train_ = dh.normalize(train)
+    test_ = dh.normalize(test)
 
-    set_seeds()
-    model = create_model(optimizer=optimizer, hl=2, hu=128, regularize=True, dropout=True, reg=l2(0.001))
-    history = model.fit(train_[cols], train['d'], epochs=50,
-                        verbose=False,
-                        class_weight=cw(train),
-                        shuffle=False,
-                        validation_split=0.2)
+    # Set random seeds
+    dh.set_seeds()
+
+    # Bagging
+
+    model = dnnm.create_model(optimizer=optimizer,
+                              reg=l2(0.001),
+                              cols=cols,
+                              hl=2,
+                              hu=128,
+                              regularize=regularize,
+                              dropout=dropout)
+
+    if bagging:
+        model_bag = dnnm.create_model_bag(model=model,
+                                          train_=train_,
+                                          train=train,
+                                          test_=test,
+                                          test=test,
+                                          dropout=dropout,
+                                          regularize=regularize,
+                                          cols=cols,
+                                          max_features=max_features)
+
+    history = dnnm.model_fit(model=model,
+                             train_=train_,
+                             train=train,
+                             cols=cols)
 
     # Evaluate in-sample performance
-    logger.info('Evaluation in-sample performance:')
-    model.evaluate(train_[cols], train['d'])
-
-    # Normalize test data set
-    test_ = (test - mu) / std
+    dnnm.evaluate(model,
+                  train_[cols],
+                  train,
+                  'in-sample')
 
     # Evaluate out-of-sample performance
-    logger.info('Evaluation out-of-sample performance:')
-    model.evaluate(test_[cols], test['d'])
+    dnnm.evaluate(model,
+                  test_[cols],
+                  test['d'],
+                  'out-of-sample')
 
-    test['p'] = np.where(model.predict(test_[cols]) > 0.5, 1, 0)
-    logger.info(test['p'].value_counts())
+    # Prediction
+    dnnm.predict(model,
+                 test,
+                 test_[cols])
 
-    res = pd.DataFrame(history.history)
-    res[['accuracy', 'val_accuracy']].plot(figsize=(10, 6), style='--')
-    plt.show()
+    # Plot accuracy
+    plt.accuracy(history)
--- data_handling/data_handling.py
+++ data_handling/data_handling.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import random
+import tensorflow as tf
 
 
 def add_lags(data: pd.DataFrame,
@@ -27,3 +29,36 @@ def add_lags(data: pd.DataFrame,
     df.dropna(inplace=True)
 
     return df, cols
+
+
+def split_train_test(data):
+    # Split the whole data set into training data and test data
+    split = int(len(data) * 0.8)
+    train = data.iloc[:split].copy()
+    test = data.iloc[split:].copy()
+
+    return train, test
+
+
+def cw(df):
+    c0, c1 = np.bincount(df['d'])
+    w0 = (1 / c0) * len(df) / 2
+    w1 = (1 / c1) * len(df) / 2
+    return {0: w0, 1: w1}
+
+
+def set_seeds(seed=1000):
+    # Python numpy and tensorflow random seeds
+    random.seed(seed)
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
+
+
+def normalize(data):
+    # Mean and standard deviation for all features
+    mu, std = data.mean(), data.std()
+
+    # Normalize training data
+    norm = (data - mu) / std
+
+    return norm
